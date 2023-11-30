7343fb7781fda29e3ccf33d69de85255544fe4d8
LorenzoAblis <lablis228609@gmail.com>
2023-11-30 11:14:07 -0600

    signup created

---
+++ client/src/Signup.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./global.css";
+
+import { Form, Button } from "react-bootstrap";
+
+const Signup = () => {
+  return (
+    <>
+      Signup
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Control type="text" placeholder="Username" name="location" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control type="text" placeholder="Email" name="date" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control type="text" placeholder="Password" name="opponent" />
+        </Form.Group>
+      </Form>
+      <Button variant="primary">Signup</Button>
+    </>
+  );
+};
+
+export default Signup;
