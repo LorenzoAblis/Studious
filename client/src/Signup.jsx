2ee32e3b56085f5b705528f7016efb68eb3c50c6
LorenzoAblis <lablis228609@gmail.com>
2023-12-04 11:16:33 -0600

    iwjdiidjewide

--- client/src/Signup.jsx
+++ client/src/Signup.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import "./global.css";
-
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
+import "./Signup.css";
 
 const Signup = () => {
   return (
     <>
-      Signupa
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Username" name="location" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Email" name="date" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Control type="text" placeholder="Password" name="opponent" />
-        </Form.Group>
-      </Form>
-      <Button variant="primary">Signup</Button>
+      <div className="form-container mt-5">
+        <h1 className="mb-4 aero-title">Aero</h1>
+        <h2>Signup</h2>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Username" name="username" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Email" name="email" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control type="text" placeholder="Password" name="password" />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              placeholder="Confirm password"
+              name="confirmPassword"
+            />
+          </Form.Group>
+        </Form>
+        <Button variant="primary" className="w-100">
+          Register
+        </Button>
+
+        <div className="mt-3">
+          <h6>
+            Already have an account? <a href="">Login</a>
+          </h6>
+        </div>
+      </div>
     </>
   );
 };
