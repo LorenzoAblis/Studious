65057a994a54726b9c150ddc79e4b0e36ca7f2b9
LorenzoAblis <lablis228609@gmail.com>
2023-12-06 11:15:36 -0600

    Implemented Login page as well as tweaks with signup page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Signup from "./pages/Signup.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Login from "./pages/Login.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Signup />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/users/signup" element={<Signup />}></Route>
+        <Route path="/users/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
