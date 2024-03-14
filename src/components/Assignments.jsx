1753386aecc4cc77ad6b6e583e2a57f13b6aa6f4
EnzoAblis <112990234+EnzoAblis@users.noreply.github.com>
2024-03-13 20:59:47 -0500

    auishduiahduiaduiahduiahduiahduiasuidahuisdhauisdhauisdhuashduiashd

--- src/components/Assignments.jsx
+++ src/components/Assignments.jsx
@@ -1,25 +1,88 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
 import "../styles/Assignments.scss";
 
+const AssignmentCard = ({
+  title,
+  className,
+  dueDate,
+  toggleDropdown,
+  showDropdown,
+}) => {
+  const handleClick = () => {
+    toggleDropdown();
+  };
+
+  return (
+    <article className="assignment-card">
+      <h2>{title}</h2>
+      <h3>Class</h3>
+      <p>{className}</p>
+      <h3>Due Date</h3>
+      <p>{dueDate}</p>
+      <div className="assignment-card-button-group">
+        <button>
+          <i className="bi bi-check-lg"></i>
+        </button>
+        <div>
+          <button onClick={handleClick}>
+            <i className="bi bi-three-dots"></i>
+          </button>
+          {showDropdown && (
+            <div className="dropdown">
+              <p>Dropdown content for {title}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </article>
+  );
+};
+
+AssignmentCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  dueDate: PropTypes.string.isRequired,
+  toggleDropdown: PropTypes.func.isRequired,
+  showDropdown: PropTypes.bool.isRequired,
+};
+
 const Assignments = () => {
+  const assignmentsData = [
+    {
+      title: "Day 12 CW",
+      className: "HPrecalculus",
+      dueDate: "Wednesday, February 28",
+    },
+    {
+      title: "Day 13 CW",
+      className: "APrecalculus",
+      dueDate: "Thursday, March 1",
+    },
+  ];
+  const [dropdownStates, setDropdownStates] = useState(
+    Array(assignmentsData.length).fill(false)
+  );
+
+  const toggleDropdown = (index) => {
+    const newDropdownStates = Array(dropdownStates.length).fill(false);
+    newDropdownStates[index] = !dropdownStates[index];
+    setDropdownStates(newDropdownStates);
+  };
+
   return (
     <section className="assignments">
       <h1>Assignments</h1>
       <div className="assignment-cards">
-        <article className="assignment-card">
-          <h2>Day 12 CW</h2>
-          <h3>Class</h3>
-          <p>HPrecalculus</p>
-          <h3>Due Date</h3>
-          <p>Wednesday, Feburary 28</p>
-          <div className="assignment-card-button-group">
-            <button>
-              <i className="bi bi-check-lg"></i>
-            </button>
-            <button>
-              <i className="bi bi-three-dots"></i>
-            </button>
-          </div>
-        </article>
+        {assignmentsData.map((assignment, index) => (
+          <AssignmentCard
+            key={index}
+            {...assignment}
+            toggleDropdown={() => toggleDropdown(index)}
+            showDropdown={dropdownStates[index]}
+          />
+        ))}
       </div>
     </section>
   );
